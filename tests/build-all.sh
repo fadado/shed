440fd307ea8ca2421c81456ef0dda3365680cc81
Joan Josep Ordinas Rosa <jordinas@gmail.com>
2015-10-12 09:11:44 +0200

    better renaming of latest tag

---
+++ tests/build-all.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+time for img in $(find -maxdepth 1 -type d | sort); do
+	[[ $img == . ]] && continue
+	echo -e "\n>>>> Building ${img#./} <<<<"
+	cd $img
+	make build
+	cd ..
+done
+
+exit
+
+# vim:syntax=sh:ai:sw=4:ts=4
