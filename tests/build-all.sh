6cc61c61dd84543274b897405a8540d7401709aa
Joan Josep Ordinas Rosa <jordinas@gmail.com>
2015-10-15 18:55:12 +0200

    practical build-all script

--- tests/build-all.sh
+++ tests/build-all.sh
@@ -2,6 +2,7 @@
 
 time for img in $(find -maxdepth 1 -type d | sort); do
 	[[ $img == . ]] && continue
+	[[ ! -e $img/Shedfile ]] && continue
 	echo -e "\n>>>> Building ${img#./} <<<<"
 	cd $img
 	make build
